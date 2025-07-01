4a98e56f0e0e98f073c69b65fa887066c86a0605
vinimari <viniciusmarinho44@hotmail.com>
2025-07-01 12:06:11 -0300

    test(OrderService): add exception scenarios for NotFound and AlreadyProcessed orders

--- java_backend/src/test/java/vs_fundos/challenge/service/OrderConsumerServiceTest.java
+++ java_backend/src/test/java/vs_fundos/challenge/service/OrderConsumerServiceTest.java
@@ -7,6 +7,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import vs_fundos.challenge.dto.OrderDTO;
+import vs_fundos.challenge.exception.OrderAlreadyProcessedException;
+import vs_fundos.challenge.exception.OrderNotFoundException;
 
 import static org.mockito.Mockito.*;
 
@@ -41,5 +43,31 @@ public class OrderConsumerServiceTest {
 
         verify(objectMapper, times(1)).readValue(invalidJsonMessage, OrderDTO.class);
         verify(orderService, never()).processOrder(anyString());
+    }
+
+    @Test
+    void shouldHandleOrderNotFoundException() throws Exception {
+        String testMessage = "{\"orderNumber\":\"ORDER-404\"}";
+        OrderDTO mockOrderDTO = OrderDTO.builder().orderNumber("ORDER-404").build();
+        when(objectMapper.readValue(testMessage, OrderDTO.class)).thenReturn(mockOrderDTO);
+        doThrow(new OrderNotFoundException("ORDER-404")).when(orderService).processOrder("ORDER-404");
+
+        orderConsumerService.listen(testMessage);
+
+        verify(objectMapper, times(1)).readValue(testMessage, OrderDTO.class);
+        verify(orderService, times(1)).processOrder("ORDER-404");
+    }
+
+    @Test
+    void shouldHandleOrderAlreadyProcessedException() throws Exception {
+        String testMessage = "{\"orderNumber\":\"ORDER-404\"}";
+        OrderDTO mockOrderDTO = OrderDTO.builder().orderNumber("ORDER-404").build();
+        when(objectMapper.readValue(testMessage, OrderDTO.class)).thenReturn(mockOrderDTO);
+        doThrow(new OrderAlreadyProcessedException("ORDER-404")).when(orderService).processOrder("ORDER-404");
+
+        orderConsumerService.listen(testMessage);
+
+        verify(objectMapper, times(1)).readValue(testMessage, OrderDTO.class);
+        verify(orderService, times(1)).processOrder("ORDER-404");
     }
 }
