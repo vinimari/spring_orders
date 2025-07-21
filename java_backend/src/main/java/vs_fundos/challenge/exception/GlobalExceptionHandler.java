fe7fadf43926f85aebf431ddde4442d2cae4c896
vinimari <viniciusmarinho44@hotmail.com>
2025-07-21 09:30:02 -0300

    test: cover custom service and advice exceptions

--- java_backend/src/main/java/vs_fundos/challenge/exception/GlobalExceptionHandler.java
+++ java_backend/src/main/java/vs_fundos/challenge/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package vs_fundos.challenge.exception;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,6 +21,21 @@ public class GlobalExceptionHandler {
         body.put("error", error);
         body.put("message", message);
         return new ResponseEntity<>(body, status);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, "Malformed request payload or invalid data types", ex.getMessage());
+    }
+
+    @ExceptionHandler(OrderProcessingException.class)
+    public ResponseEntity<Object> handleOrderProcessingException (OrderProcessingException ex) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", ex.getMessage());
+    }
+
+    @ExceptionHandler(ResponseEncryptionException.class)
+    public ResponseEntity<Object> handleResponseEncryptionException (ResponseEncryptionException ex) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", ex.getMessage());
     }
 
     @ExceptionHandler(JsonProcessingException.class)
